b0bf0ec5c2a3d0346486a9efcebcc262e303ef13
Gabryelly-Jeniffer <gabryellyboeira@users.noreply.github.com>
2024-11-30 14:02:15 -0300

    tratando exceção

--- AtividadeFormativa/AtividadeFormativa.py
+++ AtividadeFormativa/AtividadeFormativa.py
@@ -24,87 +24,105 @@ while opcao== "A" or opcao == "B" or opcao == "C" or opcao == "D" or opcao =="E"
       match opcao:
             case "A":
                   while True:
-                        numero = fun.menu_secundario("Estudantes")
-                        if numero == 1:
-                              fun_estudante.editar_estudante(estudantes)
-                        elif numero == 2:
-                              fun.excluir_item(estudantes, "codigo_estudante")
-                        elif numero == 3:
-                              fun_estudante.cadastrar_estudante(estudantes)
-                        elif numero == 4:
-                              if not len(estudantes):
-                                    print("Não há estudantes cadastrados\n")
-                              else:
-                                    fun_estudante.listar_estudantes(estudantes)
-                        elif numero == 5:
-                              break
+
+                        try:
+                              numero = fun.menu_secundario("Estudantes")
+                              if numero == 1:
+                                    fun_estudante.editar_estudante(estudantes)
+                              elif numero == 2:
+                                    fun.excluir_item(estudantes, "codigo_estudante")
+                              elif numero == 3:
+                                    fun_estudante.cadastrar_estudante(estudantes)
+                              elif numero == 4:
+                                    if not len(estudantes):
+                                          print("Não há estudantes cadastrados\n")
+                                    else:
+                                          fun_estudante.listar_estudantes(estudantes)
+                              elif numero == 5:
+                                    break
+                        except:
+                              print("Opção inválida\n")
 
             case "B":
                   while True:
-                        numero = fun.menu_secundario("Disciplinas")
-                        if numero == 1:
-                              fun_disciplina.editar_disciplina(disciplinas)
-                        elif numero == 2:
-                              fun.excluir_item(disciplinas, "codigo_disciplina")
-                        elif numero == 3:
-                              fun_disciplina.cadastrar_disciplina(disciplinas)
-                        elif numero == 4:
-                              if not len(disciplinas):
-                                    print("Não há disciplinas cadastrados\n")
-                              else:
-                                    fun_disciplina.listar_disciplina(disciplinas)
-                        elif numero == 5:
-                              break
+                        try:
+                              numero = fun.menu_secundario("Disciplinas")
+                              if numero == 1:
+                                    fun_disciplina.editar_disciplina(disciplinas)
+                              elif numero == 2:
+                                    fun.excluir_item(disciplinas, "codigo_disciplina")
+                              elif numero == 3:
+                                    fun_disciplina.cadastrar_disciplina(disciplinas)
+                              elif numero == 4:
+                                    if not len(disciplinas):
+                                          print("Não há disciplinas cadastrados\n")
+                                    else:
+                                          fun_disciplina.listar_disciplina(disciplinas)
+                              elif numero == 5:
+                                    break
+                        except:
+                              print("Opção inválida\n")
+
             case "C":
                   while True:
-                        numero = fun.menu_secundario("Matrículas")
-                        if numero == 1:
-                              fun_matricula.editar_matricula(matriculas, estudantes, turmas)
-                        elif numero == 2:
-                              fun.excluir_item(matriculas, "codigo_matricula")
-                        elif numero == 3:
-                              fun_matricula.criar_matricula(matriculas, estudantes, turmas)
-                        elif numero == 4:
-                              if not len(matriculas):
-                                    print("Não há turmas cadastrados\n")
-                              else:
-                                    fun_matricula.listar_matricula(matriculas)
-                        elif numero == 5:
-                              break
+                        try:
+                              numero = fun.menu_secundario("Matrículas")
+                              if numero == 1:
+                                    fun_matricula.editar_matricula(matriculas, estudantes, turmas)
+                              elif numero == 2:
+                                    fun.excluir_item(matriculas, "codigo_matricula")
+                              elif numero == 3:
+                                    fun_matricula.criar_matricula(matriculas, estudantes, turmas)
+                              elif numero == 4:
+                                    if not len(matriculas):
+                                          print("Não há turmas cadastrados\n")
+                                    else:
+                                          fun_matricula.listar_matricula(matriculas)
+                              elif numero == 5:
+                                    break
+                        except:
+                              print("Opção inválida\n")
+
             case "D":
                   while True:
-                        numero = fun.menu_secundario("Professor")
-                        if numero == 1:
-                              fun_professor.editar_professor(professores)
-                        elif numero == 2:
-                              fun.excluir_item(professores, "codigo_professor")
-                        elif numero == 3:
-                              (fun_professor.cadastrar_professor(professores))
-                        elif numero == 4:
-                              if not len(professores):
-                                    print("Não há professores cadastrados\n")
-                              else:
-                                    fun_professor.listar_professor(professores)
-                        elif numero == 5:
-                              break
+                        try:
+                              numero = fun.menu_secundario("Professor")
+                              if numero == 1:
+                                    fun_professor.editar_professor(professores)
+                              elif numero == 2:
+                                    fun.excluir_item(professores, "codigo_professor")
+                              elif numero == 3:
+                                    (fun_professor.cadastrar_professor(professores))
+                              elif numero == 4:
+                                    if not len(professores):
+                                          print("Não há professores cadastrados\n")
+                                    else:
+                                          fun_professor.listar_professor(professores)
+                              elif numero == 5:
+                                    break
+                        except:
+                              print("Opção inválida\n")
+
             case "E":
                   while True:
-                        numero = fun.menu_secundario("Turmas")
-                        if numero == 1:
-                              fun_turma.editar_turma(turmas, professores, disciplinas)
-                        elif numero == 2:
-                              fun.excluir_item(turmas, "codigo_turma")
-                        elif numero == 3:
-                              fun_turma.criar_turma(turmas, professores, disciplinas)
-                        elif numero == 4:
-                              if not len(turmas):
-                                    print("Não há turas cadastrados\n")
-                              else:
-                                    fun_turma.listar_turma(turmas)
-                        elif numero == 5:
-                              break
-            case _:
-                  print("EM DESENVOLVIMENTO\n")
+                        try:
+                              numero = fun.menu_secundario("Turmas")
+                              if numero == 1:
+                                    fun_turma.editar_turma(turmas, professores, disciplinas)
+                              elif numero == 2:
+                                    fun.excluir_item(turmas, "codigo_turma")
+                              elif numero == 3:
+                                    fun_turma.criar_turma(turmas, professores, disciplinas)
+                              elif numero == 4:
+                                    if not len(turmas):
+                                          print("Não há turas cadastrados\n")
+                                    else:
+                                          fun_turma.listar_turma(turmas)
+                              elif numero == 5:
+                                    break
+                        except:
+                              print("Opção inválida\n")
+
 
       opcao = fun.menu_principal()
 
